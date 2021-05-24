130d687156661a64f0f8cf0e8ab9c119cb9bd49a
monoue <like_a_newborn_baby@icloud.com>
2021-05-25 03:05:39 +0900

    improve: divided into functions

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -2,33 +2,37 @@
 
 # frozen_string_literal: true
 
-scores = ARGV[0].split(',').map do |arg|
-  if arg == 'X'
-    10
-  else
-    arg.to_i
-  end
-end
-
-bonus_score = 0
-first_throw = true
-frame_count = 1
-
-scores.each_with_index do |score, i|
-  break if frame_count == 10
-
-  if first_throw
-    if score == 10
-      frame_count += 1
-      bonus_score += scores[i + 1 .. i + 2].sum
+def turn_arg_into_int_arr
+  ARGV[0].split(',').map do |arg|
+    if arg == 'X'
+      10
     else
-      first_throw = false
+      arg.to_i
     end
-  else
-    bonus_score += scores[i + 1] if score + scores[i - 1] == 10
-    frame_count += 1
-    first_throw = true
   end
 end
 
-p scores.sum + bonus_score
+def calc_bonus_score(scores)
+  bonus_score = 0
+  first_throw = true
+  frame_count = 1
+  scores.each_with_index do |score, i|
+    return bonus_score if frame_count == 10
+
+    if first_throw
+      if score == 10
+        frame_count += 1
+        bonus_score += scores[i + 1..i + 2].sum
+      else
+        first_throw = false
+      end
+    else
+      bonus_score += scores[i + 1] if score + scores[i - 1] == 10
+      frame_count += 1
+      first_throw = true
+    end
+  end
+end
+
+scores = turn_arg_into_int_arr
+puts scores.sum + calc_bonus_score(scores)
