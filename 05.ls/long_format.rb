9b05e039a92ff9df9539bcbb5ee924cc5b2cf0ee
monoue <like_a_newborn_baby@icloud.com>
2021-06-01 10:38:42 +0900

    add: Long format, from permission until mtime, except for Mac extended attributes

--- 05.ls/long_format.rb
+++ 05.ls/long_format.rb
@@ -1,29 +1,94 @@
-file_info = File.lstat('/bin/cat')
+
+require 'etc'
+
 def get_file_type(file_info)
-  case file_info.ftype
-  when 'blockSpecial'
-    'b'
-  when 'characterSpecial'
-    'c'
-  when 'directory'
-    'd'
-  when 'link'
-    'l'
-  when 'socket'
-    's'
-  when 'fifo'
-    'p'
-  when 'file'
-    '-'
-  else
-    ' '
+  if file_info.blockdev? then 'b'
+  elsif file_info.chardev? then 'c'
+  elsif file_info.directory? then 'd'
+  elsif file_info.symlink? then 'l'
+  elsif file_info.socket? then 's'
+  elsif file_info.pipe? then 'p'
+  elsif file_info.file? then '-'
+  else ' '
   end
 end
 
-def get_owner_permission(file_info)
+module Permission
+  class << self
+    def char_bit_set?(c)
+      c == '1'
+    end
 
+    def get_readable_and_writable_s(permission_binary_s)
+      readable_c = char_bit_set?(permission_binary_s[-3]) ? 'r' : '-'
+      writable_c = char_bit_set?(permission_binary_s[-2]) ? 'w' : '-'
+      "#{readable_c}#{writable_c}"
+    end
+
+    def get_executable_or_not_char(permission_binary_c)
+      char_bit_set?(permission_binary_c) ? 'x' : '-'
+    end
+
+    def get_owner_or_group_permission(permission_binary_s, set_user_or_group_id)
+      readable_and_writable_s = get_readable_and_writable_s(permission_binary_s)
+      if set_user_or_group_id
+        "#{readable_and_writable_s}#{char_bit_set?(permission_binary_s[-1]) ? 's' : 'S'}"
+      else
+        "#{readable_and_writable_s}#{get_executable_or_not_char(permission_binary_s[-1])}"
+      end
+    end
+
+    def get_other_permission(permission_binary_s, sticky)
+      readable_and_writable_s = get_readable_and_writable_s(permission_binary_s)
+      if sticky
+        "#{readable_and_writable_s}#{char_bit_set?(permission_binary_s[-1]) ? 't' : 'T'}"
+      else
+        "#{readable_and_writable_s}#{get_executable_or_not_char(permission_binary_s[-1])}"
+      end
+    end
+  end
+
+  private_class_method :char_bit_set?, :get_readable_and_writable_s, :get_owner_or_group_permission, :get_other_permission
+
+  def self.get_permission(file_info)
+    mode_octal = file_info.mode.to_s(8)
+    permission_strs = {}
+    permission_strs[:owner] = get_owner_or_group_permission(mode_octal[-3].to_i.to_s(2), file_info.setuid?)
+    permission_strs[:group] = get_owner_or_group_permission(mode_octal[-2].to_i.to_s(2), file_info.setgid?)
+    permission_strs[:other] = get_other_permission(mode_octal[-1].to_i.to_s(2), file_info.sticky?)
+    permission_strs.values.join
+  end
 end
 
-def get_permission(file_info)
-  "#{get_owner_permission(file_info)}#{get_group_permission(file_info)}#{get_other_permission(file_info)}"
+def get_mode_block(file_info)
+  "#{get_file_type(file_info)}#{Permission.get_permission(file_info)}"
+end
+
+def get_user_name(file_info)
+  Etc.getpwuid(file_info.uid).name
+end
+
+def get_group_name(file_info)
+  Etc.getgrgid(file_info.gid).name
+end
+
+def recent?(file_mtime)
+  return false if file_mtime > Time.now
+  six_month = 60 * 60 * 24 * 365.2425 / 2
+  file_mtime.to_f >= Time.now.to_f - six_month
+end
+
+def get_time_or_year(file_mtime)
+  if recent?(file_mtime)
+    format '%0d:%0d', file_mtime.hour, file_mtime.min
+  else
+    format '%5d', file_mtime.year
+  end
+end
+
+def get_time_stamp(file_mtime)
+  month = format '%2d', file_mtime.month
+  day = format '%2d', file_mtime.day
+  time_or_year = get_time_or_year(file_mtime)
+  "#{month} #{day} #{time_or_year}"
 end
--- 05.ls/test/long_format_test.rb
+++ 05.ls/test/long_format_test.rb
@@ -1,7 +1,7 @@
 require 'minitest/autorun'
 require_relative '../long_format'
 
-class TestLongFormat < Minitest::Test
+class TestLongFormatFileType < Minitest::Test
   def test_long_format_regular_file
     file_info = File.lstat('/bin/cat')
     assert_equal '-', get_file_type(file_info)
@@ -12,3 +12,80 @@ class TestLongFormat < Minitest::Test
     assert_equal 'd', get_file_type(file_info)
   end
 end
+
+class TestLongFormatPermission < Minitest::Test
+  def test_permission1
+    file_info = File.lstat('/iSCSI')
+    assert_equal 'rwxrwxrwt', Permission.get_permission(file_info)
+  end
+
+  def test_permission2
+    file_info = File.lstat('/Applications')
+    assert_equal 'rwxrwxr-x', Permission.get_permission(file_info)
+  end
+
+  def test_permission3
+    file_info = File.lstat('/goinfre')
+    assert_equal 'rwxrwxrwt', Permission.get_permission(file_info)
+  end
+
+  def test_permission4
+    file_info = File.lstat('/dev')
+    assert_equal 'r-xr-xr-x', Permission.get_permission(file_info)
+  end
+
+  def test_permission5
+    file_info = File.lstat('/sgoinfre')
+    assert_equal 'r-xr-xr-x', Permission.get_permission(file_info)
+  end
+end
+
+class TestUserName < Minitest::Test
+  def test_user_name1
+    file_info = File.lstat('/iSCSI')
+    assert_equal 'root', get_user_name(file_info)
+  end
+
+  def test_user_name2
+    file_info = File.lstat('/exam')
+    assert_equal 'exam', get_user_name(file_info)
+  end
+end
+
+class TestGroupName < Minitest::Test
+  def test_group_name1
+    file_info = File.lstat('/iSCSI')
+    assert_equal 'wheel', get_group_name(file_info)
+  end
+
+  def test_group_name2
+    file_info = File.lstat('/exam')
+    assert_equal 'applications', get_group_name(file_info)
+  end
+end
+
+class TestFileSize < Minitest::Test
+  def test_file_size1
+    file_info = File.lstat('/iSCSI')
+    assert_equal 64, file_info.size
+  end
+
+  def test_group_name2
+    file_info = File.lstat('/exam')
+    assert_equal 576, file_info.size
+  end
+end
+
+class TestModificationTime < Minitest::Test
+  def test_mtime1
+    file_info = File.lstat('/iSCSI')
+    assert_equal ' 1  6  2020', get_time_stamp(file_info.mtime)
+  end
+
+  def test_mtime2
+    file_info = File.lstat('/Applications')
+    assert_equal ' 4 19 14:59', get_time_stamp(file_info.mtime)
+  end
+end
+
+
