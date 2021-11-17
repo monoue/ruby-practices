e6c83e4150ed78d24612ead2b7d9fb7e784b9103
Makoto Onoue <like_a_newborn_baby@icloud.com>
2021-11-17 13:33:51 +0900

    improve: readability

--- 08.ls_object/lib/sections/normal_format_files_section.rb
+++ 08.ls_object/lib/sections/normal_format_files_section.rb
@@ -15,17 +15,16 @@ module Sections
       file_statuses = filenames.map do |filename|
         FileStatus.new(filename: filename)
       end
-      width_for_filename = calculate_width_for_filename(file_statuses)
-      filenames_num_per_line = IO.console.winsize[1] / width_for_filename
+      multibyte_width_for_filename = calculate_multibyte_width_for_filename(file_statuses)
+      filenames_num_per_line = IO.console.winsize[1] / multibyte_width_for_filename
       rows_num = (file_statuses.size / filenames_num_per_line.to_f).ceil
       rows = []
       (0...rows_num).each do |current_row|
         cols = []
         current_row.step(file_statuses.size - 1, rows_num) do |i|
-          cols << format(
-            '%-*s',
-            width_for_filename - file_statuses[i].multibyte_chars_of_filename_num, file_statuses[i].filename
-          )
+          file_status = file_statuses[i]
+          width_for_filename = multibyte_width_for_filename - file_status.multibyte_chars_of_filename_num
+          cols << format('%-*s', width_for_filename, file_status.filename)
         end
         rows << cols.join.rstrip
       end
@@ -36,7 +35,7 @@ module Sections
 
     attr_reader :filenames, :directory_path
 
-    def calculate_width_for_filename(file_statuses)
+    def calculate_multibyte_width_for_filename(file_statuses)
       file_statuses.map(&:multibyte_filename_length).max + 1
     end
   end
